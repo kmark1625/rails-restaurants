5348f2a025357cb1bbb90097837fe7977a338f05
Kevin Mark <kmark1625@gmail.com>
2016-02-02 17:03:03 -0600

    Add controller for an order

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :inputfiles, only: [:index, :new, :create, :destroy]
 
+  resources :orders, only: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
