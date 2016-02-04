c2285ad3794ee07814990077047bc304f48f50fe
Kevin Mark <kmark1625@gmail.com>
2016-02-04 10:43:35 -0600

    Add inputfiles for sample

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,4 +1,5 @@
 class HelpController < ApplicationController
   def index
+    @inputfiles = Inputfile.all
   end
 end
--- db/migrate/20160202181552_create_inputfiles.rb
+++ db/migrate/20160202181552_create_inputfiles.rb
@@ -2,6 +2,7 @@ class CreateInputfiles < ActiveRecord::Migration
   def change
     create_table :inputfiles do |t|
       t.string :name
+      t.string :description
       t.string :attachment
 
       t.timestamps null: false
