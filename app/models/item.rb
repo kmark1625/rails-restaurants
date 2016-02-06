93e7ee808c363b45c732880fed35373971f156e5
Kevin Mark <kmark1625@gmail.com>
2016-02-06 16:03:26 -0600

    Add Active Model validations to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,29 @@
 class Item
+  include ActiveModel::Validations
+
   attr_accessor :name, :price, :quantity
 
-  def initialize(name, price)
-    @name = name
-    @price = price
+  validates :name, :presence => true
+  validates :price, :presence => true
+
+  # def initialize(name, price)
+  #   @name = name
+  #   @price = price
+  #   @quantity = 1
+  # end
+
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
     @quantity = 1
   end
 
   def price_in_cents
     return (price * 100).to_i
+  end
+
+  def persisted?
+    false
   end
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.describe Menu, :type => :model do
   before(:each) do
-    item1 = Item.new("mixed fruit", 2.15)
-    item2 = Item.new("french fries", 2.75)
-    item3 = Item.new("side salad", 3.35)
-    item4 = Item.new("hot wings", 3.55)
-    item5 = Item.new("mozzarella sticks", 4.20)
-    item6 = Item.new("sampler plate", 5.80)
+    item1 = Item.new(name: "mixed fruit", price: 2.15)
+    item2 = Item.new(name: "french fries", price: 2.75)
+    item3 = Item.new(name: "side salad", price: 3.35)
+    item4 = Item.new(name: "hot wings", price: 3.55)
+    item5 = Item.new(name: "mozzarella sticks", price: 4.20)
+    item6 = Item.new(name: "sampler plate", price: 5.80)
     item_array = [item1, item2, item3, item4, item5, item6]
     @menu = Menu.new(15.05, item_array)
   end
